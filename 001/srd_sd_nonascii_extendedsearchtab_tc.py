9926a67db60516ef8d5dd46f50912615dc98e54f
crozarakamilla <crozarakamilla@p852573.(none)>
2013-11-18 11:13:44 -0500

    adding selenese examples

--- 001/srd_sd_nonascii_extendedsearchtab_tc.py
+++ 001/srd_sd_nonascii_extendedsearchtab_tc.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
-# -*- coding: utf-8 *-*
-
-
 from selenium import webdriver
-import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+import unittest, time, re
 
 class SrdSdNonasciiExtendedsearchtabTc(unittest.TestCase):
     def setUp(self):
@@ -15,32 +15,72 @@ class SrdSdNonasciiExtendedsearchtabTc(unittest.TestCase):
     
     def test_srd_sd_nonascii_extendedsearchtab_tc(self):
         driver = self.driver
-        driver.get(self.base_url + "SRD/search.php?extended")
-        self.assertEqual("SAMATE Reference Dataset", driver.title)
+        driver.get(self.base_url + "/ngSRD/view.php")
+        driver.find_element_by_xpath("//a[contains(text(),'SRD Home')]").click()
+        driver.find_element_by_xpath("//a[contains(text(),'Search / Download')]").click()
+        driver.find_element_by_xpath("//a[contains(text(),'Extended Search')]").click()
+        try: self.assertEqual("SAMATE Reference Dataset", driver.title)
+        except AssertionError as e: self.verificationErrors.append(str(e))
         driver.find_element_by_xpath("//input[@name='reference']").send_keys(u"ñó? ä?çíì ??/??  ??/?? Huáy?; ?? Zh?ngwén ???????? Lech Wa??sa æøå")
         driver.find_element_by_xpath("//input[@name='Submit']").click()
         self.assertEqual("SAMATE Reference Dataset :: View all test cases", driver.title)
-        self.assertEqual("Test Case ID", driver.find_element_by_xpath("//div[@id='content']/form/table/tbody/tr/td[2]").text)
-        self.assertEqual("Description", driver.find_element_by_xpath("//div[@id='content']/form/table/tbody/tr/td[7]").text)
-        self.assertEqual("Weakness", driver.find_element_by_xpath("//div[@id='content']/form/table/tbody/tr/td[8]").text)
+        try: self.assertEqual("Test Case ID", driver.find_element_by_xpath("//div[@id='content']/form/table/tbody/tr/td[2]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("Description", driver.find_element_by_xpath("//div[@id='content']/form/table/tbody/tr/td[7]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("Weakness", driver.find_element_by_xpath("//div[@id='content']/form/table/tbody/tr/td[8]").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
         driver.back()
         driver.find_element_by_xpath("//input[@name='description']").send_keys(u"ñó? ä?çíì ??/??  ??/?? Huáy?; ?? Zh?ngwén ???????? Lech Wa??sa æøå")
         driver.find_element_by_xpath("//input[@name='Submit']").click()
         self.assertEqual("SAMATE Reference Dataset :: View all test cases", driver.title)
-        self.assertEqual("View/Download Downloads:      \n \n There is no such test case in the database! Back to the previous page", driver.find_element_by_xpath("//div[@id='content']").text)    
-        self.assertEqual("Back to the previous page", driver.find_element_by_link_text("Back to the previous page").text)
+        try: self.assertEqual("View/Download Downloads:      \n \n There is no such test case in the database! Back to the previous page", driver.find_element_by_xpath("//div[@id='content']").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("Back to the previous page", driver.find_element_by_link_text("Back to the previous page").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
         driver.back()
         driver.find_element_by_xpath("//input[@name='author']").send_keys(u"ñó? ä?çíì ??/??  ??/?? Huáy?; ?? Zh?ngwén ???????? Lech Wa??sa æøå")
         driver.find_element_by_xpath("//input[@name='Submit']").click()
         self.assertEqual("SAMATE Reference Dataset :: View all test cases", driver.title)
+        try: self.assertEqual("View/Download Downloads:      \n \n There is no such test case in the database! Back to the previous page", driver.find_element_by_xpath("//div[@id='content']").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("Back to the previous page", driver.find_element_by_link_text("Back to the previous page").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
         driver.back()
         driver.find_element_by_xpath("//input[@name='contributor']").send_keys(u"ñó? ä?çíì ??/??  ??/?? Huáy?; ?? Zh?ngwén ???????? Lech Wa??sa æøå")
         driver.find_element_by_xpath("//input[@name='Submit']").click()
         self.assertEqual("SAMATE Reference Dataset :: View all test cases", driver.title)
+        try: self.assertEqual("View/Download Downloads:      \n \n There is no such test case in the database! Back to the previous page", driver.find_element_by_xpath("//div[@id='content']").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        try: self.assertEqual("Back to the previous page", driver.find_element_by_link_text("Back to the previous page").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
         driver.back()
+        driver.get(self.base_url + "")
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
     
     def tearDown(self):
         self.driver.quit()
-    
+        self.assertEqual([], self.verificationErrors)
+
 if __name__ == "__main__":
     unittest.main()
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-selenium_presentation
+Selenium Presentation
 =====================
+
+# Demo test cases description #
+
+### Test 1 - ASCII Special Characters in UI ###
+
+For each field insert the following values:
+
+ - "This sentence is wrapped in double quotes."
+ - 'single quotes'
+ - [square brackets]
+ - {curly braces}
+ - <angle brackets>
+ - \<!-- XML comment --\>
+ - "quoted" segment & ampersand
+ - "A "quoted" segment; & (entity); wrapped in quotes"
+ - \\c:\\mydocs
+ - Hawai`i
+ - #20
+ - \/
+
+
+ ### Test 2 - Non-ASCII Characters in UI ###
+
+For each field insert the following text:
+
+ - ñó? ä?çíì ??/??  ??/?? Huáy?; ?? Zh?ngwén ???????? Lech Wa??sa æøå
+
+And verify if the search is performed correctly.1
